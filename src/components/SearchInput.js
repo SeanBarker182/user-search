f203a40180eddf821f9feea92feb93f04fce9f0c
Sean Barker <imseanbarker@gmail.com>
2021-01-27 11:56:03 -0500

    Adds the filtering feature

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,19 +1,41 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { AppContext } from '../context/GlobalState';
 
 export default function SearchInput() {
 	const [focused, setFocused] = useState(false);
 	const [searchText, setSearchText] = useState('');
+
+	const { userData, editAppContext } = useContext(AppContext);
 
 	const notFocused =
 		'text-lg text-gray-400 absolute pointer-events-none ease-in duration-150 top-3 left-4';
 	const isFocused =
 		'text-xs text-gray-400 absolute pointer-events-none ease-in duration-150 top-1 left-4';
 
+	function handleChange(e) {
+		setSearchText(e.target.value);
+
+		// Check if any of the values in the userData array start with the value of the input
+		const results = userData.filter((obj) =>
+			Object.keys(obj).some((key) => {
+				if (typeof obj[key] === 'string') {
+					return obj[key]
+						.toLowerCase()
+						.startsWith(e.target.value.toLowerCase());
+				} else {
+					return false;
+				}
+			})
+		);
+
+		editAppContext('userDataEdited', results);
+	}
+
 	return (
 		<div className='relative mt-2 flex-grow'>
 			<input
 				value={searchText}
-				onChange={(e) => setSearchText(e.target.value)}
+				onChange={handleChange}
 				onFocus={() => setFocused(true)}
 				onBlur={() => {
 					if (searchText === '') setFocused(false);
